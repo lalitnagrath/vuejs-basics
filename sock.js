bdf6cb4c501a3e715d1dff22d017092611971606
Lalit Nagrath <lalitnagrath@gmail.com>
2019-11-09 22:40:54 +0530

    vue JS Example

---
+++ sock.js
@@ -0,0 +1,10 @@
+var app=new Vue({
+el:"#app",
+data:{
+    product:'socks',
+    description:'this is a brand new sock',
+    image:'./71-ShN45tRL._UX522_.jpg',
+    link:'http://google.com',
+    instock:'false'
+}
+});

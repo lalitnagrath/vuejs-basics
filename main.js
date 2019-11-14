331e660e769e326a0fca163a36eca5d4d3370280
Lalit Nagrath <lalitnagrath@gmail.com>
2019-11-15 00:32:09 +0530

    vue JS Example

---
+++ main.js
@@ -0,0 +1,9 @@
+var vue=new Vue({
+    el:'#app',
+    data:{
+        product:'boots',
+        description:"Shoes for your Running and personal usage",
+        image:'71-ShN45tRL._UX522_.jpg',
+        url:'http://www.google.com'
+    }
+});
